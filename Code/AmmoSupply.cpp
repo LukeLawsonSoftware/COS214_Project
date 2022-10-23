fcadf6654ae21688a797f6b9069149a8462b3b63
Arno <u21457451@tuks.co.za>
2022-10-23 11:14:17 +0200

    Added Doxygen comments to AmmoSupply.h Added implementation to AmmoSupply.cpp

--- Code/AmmoSupply.cpp
+++ Code/AmmoSupply.cpp
@@ -2,7 +2,27 @@
 
 AmmoSupply::AmmoSupply(int factoryLevel, int quantity) {
 	// TODO - implement AmmoSupply::AmmoSupply
-	throw "Not yet implemented";
+
+	double bonus=0;
+	srand((unsigned) time(0));//to generate a different value each time
+
+	//generate bonus based on factory level
+	if (factoryLevel == 1)
+	{
+		bonus = (double) (10 + (rand() % 20)) / 100;
+	}
+	else if (factoryLevel > 1 && factoryLevel < 4)
+	{
+		bonus = (double) (20 + (rand() % 40)) / 100;
+	}
+	else
+	{
+		bonus = (double) (40 + (rand() % 80)) / 100;
+	}
+
+	setAmmoBonus(bonus*quantity);
+
+	// throw "Not yet implemented";
 }
 
 int AmmoSupply::getAmmoBonus() {
--- Code/AmmoSupply.h
+++ Code/AmmoSupply.h
@@ -2,17 +2,31 @@
 #define AMMOSUPPLY_H
 #include "Supply.h"
 
+#include <cstdlib>
+#include <ctime>
+
 class AmmoSupply : public Supply
 {
 
-public:
-	int ammoBonus;
+	public:
+		/// @brief Constructor for AmmoSupply class to specify the factory level and quantity that will be produced.
+		/// @author Arno Jooste (21457451)
+		/// @param factoryLevel Specifies the currrent factory level in order to set the multiplier of the bonus.
+		/// @param quantity Specifies the quentity of ammo supplies to be produced. This amount will be used to calculate the ammoBonus.
+		AmmoSupply(int factoryLevel, int quantity);
 
-	AmmoSupply(int factoryLevel, int quantity);
+		/// @brief Getter for the ammo bonus member variable.
+		/// @author Arno Jooste (u21457451)
+		/// @return ammo bonus of type int.
+		int getAmmoBonus();
 
-	int getAmmoBonus();
+		/// @brief Setter for the ammo bonus member variable.
+		/// @author Arno Jooste (u21457451)
+		/// @param bonus Specfies to which value the ammo bonus will be set.
+		void setAmmoBonus(int bonus);
 
-	void setAmmoBonus(int bonus);
+	private:
+		int ammoBonus;
 };
 
 #endif
