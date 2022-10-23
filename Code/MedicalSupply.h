3e8879e15ff7bd577c7a5b43e3de300b8343069d
Arno <u21457451@tuks.co.za>
2022-10-23 08:50:52 +0200

    Added Doxygen comments to MedicalSupply.h

--- Code/MedicalSupply.h
+++ Code/MedicalSupply.h
@@ -6,13 +6,25 @@ class MedicalSupply : public Supply
 {
 
 public:
-	int medicalBonus;
 
+	/// @brief Constructor for MedicalSupply class to specify the factory level and quantity that will be produced.
+	/// @author Arno Jooste (21457451)
+	/// @param factoryLevel Specifies the currrent factory level in order to set the percentage(%) of bonus
+	/// @param quantity Specifies the quentity of medical supplies to be produced. This amount will be used to calculate the medicalBonus
 	MedicalSupply(int factoryLevel, int quantity);
 
+	/// @brief Getter for the medical bonus member variable.
+	/// @author Arno Jooste (u21457451)
+	/// @return medical bonus of type int.
 	int getMedicalBonus();
 
+	/// @brief Setter for the medical bonus member variable.
+	/// @author Arno Jooste (u21457451)
+	/// @param bonus Specfies to which value the medical bonus will be set.
 	void setMedicalBonus(int bonus);
+
+private:
+	int medicalBonus;
 };
 
 #endif
