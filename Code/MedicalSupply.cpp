041b874a30bc24ff70d70fe7c9ab1ce0991e262c
Arno <u21457451@tuks.co.za>
2022-10-23 10:19:00 +0200

    Added implementation to MedicalSupply.cpp

--- Code/MedicalSupply.cpp
+++ Code/MedicalSupply.cpp
@@ -2,7 +2,28 @@
 
 MedicalSupply::MedicalSupply(int factoryLevel, int quantity) {
 	// TODO - implement MedicalSupply::MedicalSupply
-	throw "Not yet implemented";
+
+	double bonus=0;
+	srand((unsigned) time(0));//to generate a different value each time
+
+	//generate bonus based on factory level
+	if (factoryLevel == 1)
+	{
+		bonus = (double) (10 + (rand() % 20)) / 100;
+	}
+	else if (factoryLevel > 1 && factoryLevel < 4)
+	{
+		bonus = (double) (20 + (rand() % 40)) / 100;
+	}
+	else
+	{
+		bonus = (double) (40 + (rand() % 80)) / 100;
+	}
+
+	setMedicalBonus(bonus*quantity);
+
+	this->quantity = quantity;
+	// throw "Not yet implemented";
 }
 
 int MedicalSupply::getMedicalBonus() {
--- Code/MedicalSupply.h
+++ Code/MedicalSupply.h
@@ -2,6 +2,9 @@
 #define MEDICALSUPPLY_H
 #include "Supply.h"
 
+#include <cstdlib>
+#include <ctime>
+
 class MedicalSupply : public Supply
 {
 
@@ -9,7 +12,7 @@ public:
 
 	/// @brief Constructor for MedicalSupply class to specify the factory level and quantity that will be produced.
 	/// @author Arno Jooste (21457451)
-	/// @param factoryLevel Specifies the currrent factory level in order to set the percentage(%) of bonus
+	/// @param factoryLevel Specifies the currrent factory level in order to set the multiplier of the bonus.
 	/// @param quantity Specifies the quentity of medical supplies to be produced. This amount will be used to calculate the medicalBonus
 	MedicalSupply(int factoryLevel, int quantity);
 
