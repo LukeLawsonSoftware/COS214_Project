b59985f651f4c33cc9fdc7126ad513565ddf5075
ReuJste30 <91607234+ReuJste30@users.noreply.github.com>
2022-10-24 10:30:29 +0200

    Added constructor & implementation for constructor

--- Code/SeaFactory.h
+++ Code/SeaFactory.h
@@ -8,6 +8,13 @@ class SeaFactory : public UnitFactory
 {
 
 public:
+	/// @brief Constructor for SeaFactory class used to instantiate an SeaFactory object
+	/// @author Reuben Jooste (u21457060)
+	/// @param budget Starting budget of SeaFactory class
+	/// @param level Starting level of SeaFactory class
+	/// @param type Type will be "Sea" since this function creates Sea army components
+	SeaFactory(double budget, int level, std::string type = "Sea");
+
 	/// @brief Calls constructor of SeaVehicle, using level to determine powerRating.
 	/// @author Luke Lawson (u21433811)
 	/// @return pointer to newly created ArmyComponent (which will be a SeaVehicle)
