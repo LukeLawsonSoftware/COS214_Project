773962fc6276ea135cc87c5b2572352e93aa33b1
Arno <u21457451@tuks.co.za>
2022-10-30 21:37:52 +0200

    Implemented createVehicle() and createSoldier() methods

--- Code/AirFactory.cpp
+++ Code/AirFactory.cpp
@@ -1,14 +1,44 @@
 #include "AirFactory.h"
 
-AirFactory::AirFactory(double budget, int level, std::string type = "Air")
+AirFactory::AirFactory(double budget, int level, std::string type)
 	:UnitFactory(budget,level, type){}
 
 ArmyComponent *AirFactory::createVehicle()
 {
+	srand((unsigned) time(0));//to generate a different value each time
+	int cost = (double) (21 + (rand() % 26));//random number between 21 and 26 (26 excluded)
+
+	int costAfterProduction = cost + totalSpent;
+
+	if (costAfterProduction > getBudget())
+	{
+		std::cout << "NOTE: CANNOT CREATE A VEHICLE DUE TO THE AIR FACTORY'S BUDGET THAT HAS BEEN REACHED." << std::endl;
+		return nullptr;
+	}
+	else
+	{
+		std::cout << "NOTE: AIR FACTORY STILL ABLE TO CREATE MORE VEHICLES." << std::endl;
+		totalSpent += costAfterProduction;
+	}
 	return new AirVehicle(determineActualLevel());
 }
 
 ArmyComponent *AirFactory::createSoldier()
 {
+	srand((unsigned) time(0));//to generate a different value each time
+	int cost = (double) (15 + (rand() % 21));//random number between 15 and 21 (21 excluded) {COST FOR AIR SOLDIERS KEPT THE SAME FOR OTHER SOLDIERS}
+
+	int costAfterProduction = cost + totalSpent;
+
+	if (costAfterProduction > getBudget())
+	{
+		std::cout << "NOTE: CANNOT CREATE A SOLDIER DUE TO THE AIR FACTORY'S BUDGET THAT HAS BEEN REACHED." << std::endl;
+		return nullptr;
+	}
+	else
+	{
+		std::cout << "NOTE: AIR FACTORY STILL ABLE TO CREATE MORE SOLDIERS." << std::endl;
+		totalSpent += costAfterProduction;
+	}
 	return new AirUnit(determineActualLevel());
 }
