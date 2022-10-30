774ffa51b6eb1462ea029df4560182a3b5e0e988
JonelleCoertze <104712883+JonelleCoertze@users.noreply.github.com>
2022-10-30 10:19:31 +0200

    Updated EconomicState's Doxygen comments

--- Code/EconomicState.h
+++ Code/EconomicState.h
@@ -7,6 +7,7 @@ class EconomicState {
 public:
 	/// @brief randomly decide what a country can do during their turn 
 	/// @author Jonelle Coertze (u21446271)
+	/// @param country pointer to an existing Country object to have access to the country's army and alliences
 	/// @return an int corresponding with the decision
 	virtual int decideMyTurn(Country* country) = 0;
 };
--- Code/Poor.h
+++ Code/Poor.h
@@ -8,6 +8,7 @@ class Poor : public EconomicState
 public:
 	/// @brief randomly decide what a country can do during their turn
 	/// @author Jonelle Coertze (u21446271)
+	/// @param country pointer to an existing Country object to have access to the country's army and alliences
 	/// @return an int corresponding with the decision
 	int decideMyTurn(Country* country);
 };
