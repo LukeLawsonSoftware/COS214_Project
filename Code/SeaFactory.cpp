713d8c5415c775925374d4d9754dd5891d04d984
Arno <u21457451@tuks.co.za>
2022-10-30 21:41:53 +0200

    Implemented createVehicle() and createSoldier() methods.

--- Code/SeaFactory.cpp
+++ Code/SeaFactory.cpp
@@ -1,14 +1,44 @@
 #include "SeaFactory.h"
 
-SeaFactory::SeaFactory(double budget, int level, std::string type = "Sea")
+SeaFactory::SeaFactory(double budget, int level, std::string type)
 	:UnitFactory(budget,level, type){}
 
 ArmyComponent *SeaFactory::createVehicle()
 {
+	srand((unsigned) time(0));//to generate a different value each time
+	int cost = (double) (18 + (rand() % 25));//random number between 18 and 25 (25 excluded)
+
+	int costAfterProduction = cost + totalSpent;
+
+	if (costAfterProduction > getBudget())
+	{
+		std::cout << "NOTE: CANNOT CREATE A VEHICLE DUE TO THE SEA FACTORY'S BUDGET THAT HAS BEEN REACHED." << std::endl;
+		return nullptr;
+	}
+	else
+	{
+		std::cout << "NOTE: SEA FACTORY STILL ABLE TO CREATE MORE VEHICLES." << std::endl;
+		totalSpent += costAfterProduction;
+	}
 	return new SeaVehicle(determineActualLevel());
 }
 
 ArmyComponent *SeaFactory::createSoldier()
 {
+	srand((unsigned) time(0));//to generate a different value each time
+	int cost = (double) (10 + (rand() % 15));//random number between 10 and 15 (15 excluded) {SOLDIER COST KEPT THE SAME AS THE OTHER SOLDIERS}
+
+	int costAfterProduction = cost + totalSpent;
+
+	if (costAfterProduction > getBudget())
+	{
+		std::cout << "NOTE: CANNOT CREATE A SOLDIER DUE TO THE SEA FACTORY'S BUDGET THAT HAS BEEN REACHED." << std::endl;
+		return nullptr;
+	}
+	else
+	{
+		std::cout << "NOTE: SEA FACTORY STILL ABLE TO CREATE MORE SOLDIERS." << std::endl;
+		totalSpent += costAfterProduction;
+	}
 	return new SeaUnit(determineActualLevel());
 }
