c94651786b2c2408f5ea02fa25f66904d795cea7
Thomas Blendulf <thomasblendulf789@gmail.com>
2022-11-02 00:21:58 +0200

    Added startWarSim Functionality

--- Code/War.cpp
+++ Code/War.cpp
@@ -1,6 +1,17 @@
 #include "War.h"
-#include "WarPhase.h"
-#include "WarTheatre.h"
+#include "EarlyPeace.h"
+#include "AirTerrain.h"
+#include "SeaTerrain.h"
+#include "LandTerrain.h"
+#include "Country.h"
+War::War()
+{
+	warTheatres.insert(std::pair<std::string, WarTheatre *>("Land", new LandTerrain()));
+	warTheatres.insert(std::pair<std::string, WarTheatre *>("Air", new AirTerrain()));
+	warTheatres.insert(std::pair<std::string, WarTheatre *>("Sea", new SeaTerrain()));
+	phase = new EarlyPeace();
+	isActive = true;
+}
 
 void War::setupTheatres()
 {
@@ -14,27 +25,89 @@ void War::setWarPhase(WarPhase *)
 
 WarTheatre *War::getLandTheatre()
 {
-	throw "Not yet implemented";
+	return warTheatres.at("Land");
 }
 WarTheatre *War::getAirTheatre()
 {
-	throw "Not yet implemented";
+	return warTheatres.at("Air");
 }
 WarTheatre *War::getSeaTheatre()
 {
-	throw "Not yet implemented";
+	return warTheatres.at("Sea");
 }
 
 void War::changePhase()
 {
-	// TODO - implement War::changePhase
-	throw "Not yet implemented";
+	phase->handleChange(this);
 }
 
 void War::startWarSim()
 {
-	// TODO - implement War::startWarSim
-	throw "Not yet implemented";
+	while (phase->peaceChance != 0)
+	{
+		srand((unsigned)time(NULL));
+		changePhase();
+		bool peace = ((double)rand() / (double)RAND_MAX) < (phase->peaceChance);
+		if (peace == true)
+		{
+
+			stopWar();
+			return;
+		}
+	}
+
+	while (isActive)
+	{
+		for (int i = 0; i < Country::alliance1.size(); i++)
+		{
+			if (Country::alliance1.at(i)->isSurrendered() == false)
+				Country::alliance1.at(i)->takeTurn(this);
+		}
+		for (int i = 0; i < Country::alliance2.size(); i++)
+		{
+			if (Country::alliance2.at(i)->isSurrendered() == false)
+				Country::alliance2.at(i)->takeTurn(this);
+		}
+
+		warTheatres.at("Land")->conflict();
+		warTheatres.at("Sea")->conflict();
+		warTheatres.at("Air")->conflict();
+
+		bool alliance1AllSurrendered = true;
+		for (int i = 0; i < Country::alliance1.size(); i++)
+		{
+			if (Country::alliance1.at(i)->isSurrendered() == false)
+			{
+				alliance1AllSurrendered = false;
+			}
+		}
+		bool alliance2AllSurrendered = false;
+		for (int i = 0; i < Country::alliance2.size(); i++)
+		{
+			if (Country::alliance2.at(i)->isSurrendered() == false)
+			{
+				alliance2AllSurrendered = false;
+			}
+		}
+		if ((!alliance1AllSurrendered) != (!alliance2AllSurrendered))
+		{
+			isActive = false;
+		}
+		if (warTheatres.at("Land")->getContentionState() != 0 || warTheatres.at("Sea")->getContentionState() != 0 || warTheatres.at("Air")->getContentionState() != 0)
+		{
+			if (warTheatres.at("Land")->getContentionState() != 3 || warTheatres.at("Sea")->getContentionState() != 3 || warTheatres.at("Air")->getContentionState() != 3)
+			{
+				if (warTheatres.at("Land")->getContentionState() == warTheatres.at("Sea")->getContentionState() == warTheatres.at("Air")->getContentionState())
+				{
+					isActive = false;
+				}
+			}
+		}
+
+		/*
+			EDIT GDP HERE
+		*/
+	}
 }
 
 void War::startWarGame()
