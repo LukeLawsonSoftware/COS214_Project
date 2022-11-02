7c30485fd681b282ac49b2eaf3b865f26fe627bb
Reuben Jooste (u21457060) <u21457060@tuks.co.za>
2022-11-02 13:02:28 +0200

    Added null-check for parameter

--- Code/Transporter.cpp
+++ Code/Transporter.cpp
@@ -1,5 +1,8 @@
 #include "Transporter.h"
 
 void Transporter :: registerCorresponder(Corresponder* c){
+    //test if parameter is not null
+    if (c == NULL) throw std::invalid_argument("Cannot register object of type NULL to corresponderList vector.");
+    //else add the object
     corresponderList.push_back(c);
 }
