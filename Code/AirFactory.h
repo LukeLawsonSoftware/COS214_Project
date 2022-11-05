6a26b5be4e91290f7b4e2d414a84549bec6a7735
ReuJste30 <91607234+ReuJste30@users.noreply.github.com>
2022-11-05 15:10:10 +0200

    Added doxygen comments

--- Code/AirFactory.h
+++ Code/AirFactory.h
@@ -23,16 +23,19 @@ public:
 	/// @param budget Starting budget of AirFactory class
 	/// @param level Starting level of AirFactory class
 	/// @param type Type will be "Air" since this function creates Air army components
+	/// @warning The "budget" must be a positive value. The "level" must be greater than zero.
 	AirFactory(double budget, int level, std::string type = "Air");
 
 	/// @brief Calls constructor of AirVehicle, using level to determine powerRating.
 	/// @author Luke Lawson (u21433811)
 	/// @return pointer to newly created ArmyComponent (which will be a AirVehicle)
+	/// @note This function may return NULL if the budget has run out.
 	ArmyComponent *createVehicle();
 
 	/// @brief Calls constructor of AirUnit, using level to determine powerRating.
 	/// @author Luke Lawson (u21433811)
 	/// @return pointer to newly created ArmyComponent (which will be a AirUnit)
+	/// @note This function may return NULL if the budget has run out.
 	ArmyComponent *createSoldier();
 };
 
--- Code/AmmoFactory.h
+++ Code/AmmoFactory.h
@@ -7,6 +7,15 @@ class AmmoSupply;
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+/**
+ * @class AmmoFactory AmmoFactory.h
+ * @brief The AmmoFactory class is a derived class derived from the UnitFactory class
+ * @ref SupplyFactory.h "(See the definition of the SupplyFactory class)"
+ * 
+ * The AmmoFactory will be used to create Ammo Suppliess for the Country's Armies. The AmmoFactory
+ * has a method "makeSupply()" which will create the AmmoSuppply object.
+ * @note This class is ONLY used to create AmmoSupply objects
+*/
 
 class AmmoFactory : public SupplyFactory
 {
@@ -15,12 +24,15 @@ public:
 	/// @author Arno Jooste (u21457451)
 	/// @param budget The amount that can be spent to make ammo supplies.
 	/// @param type The type of the factory.
+	/// @warning "type" has to be "Ammo" and "budget" may not be less than or equal to zero.
 	AmmoFactory(int budget, std::string type);
 
 	/// @brief Creates ammo supplies by creating a new AmmoSupply product.
 	/// @author Arno Jooste (u21457451)
 	/// @param quantity The quantity of ammo supplies to be produced by the ammo factory.
 	/// @return Pointer to newly created AmmoSupply product.
+	/// @warning "quantity" need to be positive.
+	/// @note This function may return NULL if the budget has run out.
 	Supply *makeSupply(int quantity);
 };
 
