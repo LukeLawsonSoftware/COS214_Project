6bcae2033ebf753814d94c2861e737ccd0926f7c
ReuJste30 <91607234+ReuJste30@users.noreply.github.com>
2022-11-05 15:18:37 +0200

    Added doxygen comments

--- Code/LandTerrain.h
+++ Code/LandTerrain.h
@@ -2,9 +2,19 @@
 #define LANDTERRAIN_H
 #include "WarTheatre.h"
 
+/**
+ * @class LandTerrain LandTerrain.h
+ * @ref WarTheatre.h "(See the definition of the WarTheatre class)"
+ * 
+ * The LandTerrain class is a derived class derived from the WarTheatre class
+ * The LandTerrain will be used to create an Land Terrain Theatre where the war can will take place.
+ * @note This class is used to expand the War by adding a new Theatre to it.
+*/
 class LandTerrain : public WarTheatre
 {
 public:
+	/// @brief This is the default constructor of the class.
+	/// @author Jonelle Coertze (u21446271)
 	LandTerrain();
 
 private:
