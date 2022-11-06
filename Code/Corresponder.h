2ec0a9ef4d0989788a0eb7f170108956b976ea13
ReuJste30 <91607234+ReuJste30@users.noreply.github.com>
2022-11-06 10:52:32 +0200

    Added Doxygen comments

--- Code/Corresponder.h
+++ Code/Corresponder.h
@@ -3,6 +3,15 @@
 
 class Transporter;
 
+/**
+ * @class Corresponder Corresponder.h
+ * 
+ * This class is used to register a Country to its transport lines when the country object is instantiated.
+ * 
+ * @warning The transport lines are the only way a Country can transport supplies (AmmoSupply & MedicalSupply) to its armies.
+ * 
+ */
+
 class Corresponder
 {
 protected:
@@ -10,6 +19,10 @@ protected:
 	Transporter *ammoTransportLine;
 
 public:
+	/// @brief This function is used to register the Country to the ammo and medical transport lines.
+	/// @param ammoTransportLine The AmmoTransporter for the country. We use this transport line to send ammo supplies to the country's army.
+	/// @param medTransportLine The MedicalTransporter for the country. We use this transport line to send medical supplies to the country's army.
+	/// @note It is possible to pass in NULL values for both argumenys but if done so then the Country would be in a great DISADVANTAGE to the other enemy countries.
 	void regToTransport(Transporter *ammoTransportLine, Transporter *medTransportLine);
 };
 
--- Code/EarlyUnstablePeace.h
+++ Code/EarlyUnstablePeace.h
@@ -8,6 +8,9 @@ public:
     /// @brief Sets next to EarlyOpenConflict and peaceChance.
     /// @author Thomas Blendulf (u21446131)
     EarlyUnstablePeace();
+
+    /// @brief This function just shows when there was a change to the current WarPhase.
+    /// @author Thomas Blendulf (u21446131)
     void outputChange();
 };
 
